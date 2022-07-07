bb8f85ba517869e59756a3b2fdbf18f5d17f06d8
Alexander Eatman <johnathanae1995@gmail.com>
2022-07-07 00:53:58 -0400

    update

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ var exported_errors = [];
 
 function comment_find(folders, keyWords) { 
 
-    //folder that was found in folder while traversing... 
     if(typeof(folders) == 'object' && Array.isArray(folders) == false && folders.single == true) { 
 
         fs.recurseSync(folders.folder, function(filepath, relative, filename) {
@@ -52,106 +51,118 @@ function comment_find(folders, keyWords) {
 
     }
 
-    //main folder traversal
-    for(let i = 0; i < folders.length; i++) {
+    if(typeof(folders) == 'object' || Array.isArray(folders) == true) { 
 
-        if(folders[i].files == 'all') {
+        for(let i = 0; i < folders.length; i++) {
 
-            fs.recurseSync(folders[i].folder, function(filepath, relative, filename) {
+            if(folders[i].files == 'all') {
 
-                if(filename) { 
+                fs.recurseSync(folders[i].folder, function(filepath, relative, filename) {
 
-                    fs.readFileSync(filename, 'utf8', function(err, data) {
+                    if(filename) { 
 
-                        if(err) { 
-                            exported_errors.push({ 
-                                folder: filepath, 
-                                file: filename, 
-                                error: err
-                            })
-                        }
+                        fs.readFileSync(filename, 'utf8', function(err, data) {
 
-                        var string_contents = data.split(' ');
-
-                        for(let i = 0; i < string_contents.length; i++) { 
-                            for(let j = 0; j < keyWords.length; j++) { 
-                                if(string_contents[i] == `//^-^${keyWords[j]}`) { 
-                                    exported_comments.push({ 
-                                        hint: 'control f this comment to find where it is', 
-                                        folder: filepath, 
-                                        file: filename, 
-                                        comment: `//^-^${keyWords[j]}`,
-                                        message: string_contents[i+1].includes('-') ? string_contents[i+1] : 'please make sure the format of your comment key is (//^-^keyWord this-comment-is-for-adding-a-number) comments must have a - seperating the words'
-                                    })
-                                } 
+                            if(err) { 
+                                exported_errors.push({ 
+                                    folder: filepath, 
+                                    file: filename, 
+                                    error: err
+                                })
                             }
-                        }
 
-                    })
+                            var string_contents = data.split(' ');
 
-                } else { 
+                            for(let i = 0; i < string_contents.length; i++) { 
+                                for(let j = 0; j < keyWords.length; j++) { 
+                                    if(string_contents[i] == `//^-^${keyWords[j]}`) { 
+                                        exported_comments.push({ 
+                                            hint: 'control f this comment to find where it is', 
+                                            folder: filepath, 
+                                            file: filename, 
+                                            comment: `//^-^${keyWords[j]}`,
+                                            message: string_contents[i+1].includes('-') ? string_contents[i+1] : 'please make sure the format of your comment key is (//^-^keyWord this-comment-is-for-adding-a-number) comments must have a - seperating the words'
+                                        })
+                                    } 
+                                }
+                            }
 
-                    comment_find({folder: filepath, single: true}, keyWords);
+                        })
 
-                }
+                    } else { 
 
-            });
+                        comment_find({folder: filepath, single: true}, keyWords);
 
-        } else { 
+                    }
 
-            if(typeof(folders[i].files) !== 'object' || Array.isArray(folders[i].files) == false) { 
-
-                exported_errors.push({ 
-                    folder: folders[i].folder, 
-                    file: JSON.stringify(folders[i].files), 
-                    error: 'please pass an array as the argument for the files within the folder you want checked'
                 });
 
-                continue;
+            } else { 
 
-            }   
+                if(typeof(folders[i].files) !== 'object' || Array.isArray(folders[i].files) == false) { 
 
-            fs.recurseSync(folders[i].folder, folders[i].files, function(filepath, relative, filename) {
-
-                if(filename) { 
-
-                    fs.readFileSync(filename, 'utf8', function(err, data) {
-
-                        if(err) { 
-                            exported_errors.push({ 
-                                folder: filepath, 
-                                file: filename, 
-                                error: err
-                            })
-                        }
-
-                        var string_contents = data.split(' ');
-
-                        for(let i = 0; i < string_contents.length; i++) { 
-                            for(let j = 0; j < keyWords.length; j++) { 
-                                if(string_contents[i] == `//^-^${keyWords[j]}`) { 
-                                    exported_comments.push({
-                                        hint: 'control f this comment to find where it is', 
-                                        folder: filepath, 
-                                        file: filename, 
-                                        comment: `^-^${keyWords[j]}`,
-                                        message: string_contents[i+1].includes('-') ? string_contents[i+1] : 'please make sure the format of your comment key is (//^-^keyWord this-comment-is-for-adding-a-number) comments must have a - seperating the words'
-                                    })
-                                } 
-                            }
-                        }
-
+                    exported_errors.push({ 
+                        folder: folders[i].folder, 
+                        file: JSON.stringify(folders[i].files), 
+                        error: 'please pass an array as the argument for the files within the folder you want checked'
                     });
 
-                } else { 
+                    continue;
 
-                    comment_find({folder: filepath, single: true}, keyWords);
+                }   
 
-                }
+                fs.recurseSync(folders[i].folder, folders[i].files, function(filepath, relative, filename) {
 
-            });
+                    if(filename) { 
 
+                        fs.readFileSync(filename, 'utf8', function(err, data) {
+
+                            if(err) { 
+                                exported_errors.push({ 
+                                    folder: filepath, 
+                                    file: filename, 
+                                    error: err
+                                })
+                            }
+
+                            var string_contents = data.split(' ');
+
+                            for(let i = 0; i < string_contents.length; i++) { 
+                                for(let j = 0; j < keyWords.length; j++) { 
+                                    if(string_contents[i] == `//^-^${keyWords[j]}`) { 
+                                        exported_comments.push({
+                                            hint: 'control f this comment to find where it is', 
+                                            folder: filepath, 
+                                            file: filename, 
+                                            comment: `^-^${keyWords[j]}`,
+                                            message: string_contents[i+1].includes('-') ? string_contents[i+1] : 'please make sure the format of your comment key is (//^-^keyWord this-comment-is-for-adding-a-number) comments must have a - seperating the words'
+                                        })
+                                    } 
+                                }
+                            }
+
+                        });
+
+                    } else { 
+
+                        comment_find({folder: filepath, single: true}, keyWords);
+
+                    }
+
+                });
+
+            }
+            
         }
+
+    } else { 
+
+        exported_errors.push({ 
+            folder: 'array must be passed as folders', 
+            file: 'array must be passed as folders', 
+            error: 'array must be passed as folders'
+        });
+
     }
 
     const result = { comments: exported_comments, errors: exported_errors };
