ec06dba6ed9b16ec3e0f06e35a4a8e0b01ab990a
Alexander Eatman <johnathanae1995@gmail.com>
2022-07-07 13:05:51 -0400

    update

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var fs = require('file-system');
 
-function comment_find(folders, keyWords) {
+function comment_find(folders) {
 
     var exported_comments = [];
     var exported_errors = [];
@@ -9,83 +9,101 @@ function comment_find(folders, keyWords) {
 
         for(let i = 0; i < folders.length; i++) {
 
-            fs.recurseSync(folders[i].folder, (filepath, relative, filename) => {
+            if(folders[i].files == 'all') { 
+                folders[i].files = [];
+            }
 
-                if(filename) { 
+            try {
 
-                    var data = fs.readFileSync(filepath, 'utf8');
-                    data = data.split('');
-                    var line_number = 0;
+                fs.recurseSync(folders[i].folder, ...folders[i].files, (filepath, relative, filename) => { //folders[i].files will not eval when all is passed. replace all with empty
 
-                    for(let i = 0; i < data.length; i++) { 
+                    if(filename) { 
 
-                        if(data[i] == '\n') { 
-                            console.log('AWESOME');
-                            console.log(data[i]);
-                            line_number = line_number + 1;
-                        }
+                        var data = fs.readFileSync(filepath, 'utf8');
+                        data = data.split('');
+                        var line_number = 0;
 
-                        if(
-                            data[i] == '/' && 
-                            data[i+1] == '/' && 
-                            data[i+2] == '^' && 
-                            data[i+3] == '*' && 
-                            data[i+4] == '^' 
-                        ) { 
+                        for(let i = 0; i < data.length; i++) { 
 
-                            if(data[i+5] !== '(') { 
-                                continue;
+                            if(data[i] == '\n') { 
+                                line_number = line_number + 1;
                             }
 
-                            var count = 0
-                            var stop = 4000;
-                            var start = i + 5;
-                            var build_this_comment = '';
+                            if(
+                                data[i] == '/' && 
+                                data[i+1] == '/' && 
+                                data[i+2] == '^' && 
+                                data[i+3] == '*' && 
+                                data[i+4] == '^' 
+                            ) { 
 
-                            while(true) { 
-
-                                i++;
-
-                                if(data[start] == '\n') { 
-                                    line_number = line_number + 1;
+                                if(data[i+5] !== '(') { 
+                                    continue;
                                 }
 
-                                if(data[start] == ')') { 
-                                    build_this_comment += ')';
-                                    break;
+                                var count = 0
+                                var stop = 4000;
+                                var start = i + 5;
+                                var build_this_comment = '';
+
+                                while(true) { 
+
+                                    i++;
+
+                                    if(data[start] == '\n') { 
+                                        line_number = line_number + 1;
+                                    }
+
+                                    if(data[start] == ')') { 
+                                        build_this_comment += ')';
+                                        break;
+                                    }
+
+                                    if(count == stop) { 
+                                        build_this_comment += '...comments can only be 4000 characters)';
+                                        break;
+                                    }
+
+                                    build_this_comment += data[start];
+                                    start += 1;
+                                    count += 1;
+
                                 }
 
-                                if(count == stop) { 
-                                    build_this_comment += '...comments can only be 4000 characters)';
-                                    break;
-                                }
-
-                                build_this_comment += data[start];
-                                start += 1;
-                                count += 1;
+                                exported_comments.push({
+                                    line_number: line_number,
+                                    folder: filepath,
+                                    comment: build_this_comment.split('-').join(' ')// /:-/|
+                                })
 
                             }
-
-                            exported_comments.push({
-                                line_number: line_number,
-                                folder: filepath,
-                                comment: build_this_comment ///.replace('-', ' ')
-                            })
-
                         }
+
                     }
+                })
 
-                }
-            })
+            } catch(err) { 
+
+                exported_comments.push({
+                    folders: folders[i].folder,
+                    error: err
+                })
+
+            }
 
         }
     }
 
-    const result = { comments: exported_comments, errors: exported_errors };
+    var loaded_comments = exported_comments;
+    var loaded_errors = exported_errors;
+
     exported_comments = [];
     exported_errors = [];
 
-    return result
+    return { 
+        comments: loaded_comments, 
+        errors: loaded_errors
+    }
 
 }
 
--- your_example_file.js
+++ your_example_file.js
@@ -1,7 +1,7 @@
 var comment_find = require('./index');
 
 const folders = [ 
-    { folder: './example_1', files: 'all' },
+    { folder: './example_1', files: ['wow.js'] },
     { folder: './example_2', files: 'all' }, 
     { folder: './example_3', files: 'all' },
     { folder: './example_4', files: 'all' },
