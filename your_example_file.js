b9886308aafc40b15ba8895172b744fbd50762f9
Alexander Eatman <johnathanae1995@gmail.com>
2022-07-06 23:59:24 -0400

    update

--- your_example_file.js
+++ your_example_file.js
@@ -3,10 +3,10 @@ var comment_find = require('./index');
 const keyWords = ['add', 'update', 'delete']; 
 
 const folders = [ 
-    { folder: '/example_folder_1', files: 'all', traverseInsideFolder: false }, 
-    { folder: '/example_folder_2', files: 'all', traverseInsideFolder: true }, 
-    { folder: '/example_folder_3', files: ['index.js', 'account.js'], traverseInsideFolder: false },
-    { folder: '/example_folder_4', files: ['bank.js'], traverseInsideFolder: false },
+    { folder: '/example_folder_1', files: 'all' },
+    { folder: '/example_folder_2', files: 'all' }, 
+    { folder: '/example_folder_3', files: ['index.js', 'account.js'] },
+    { folder: '/example_folder_4', files: ['bank.js'] },
 ];
 
 var result = comment_find(folders, keyWords);
